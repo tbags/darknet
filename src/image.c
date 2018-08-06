4f85ba199831def961e1291f34832c3240fb5427
Shashank Hegde <shashankhegde@gmail.com>
2018-08-06 00:29:28 -0500

    config fix

--- src/image.c
+++ src/image.c
@@ -223,7 +223,7 @@ image **load_alphabet()
         alphabets[j] = calloc(128, sizeof(image));
         for(i = 32; i < 127; ++i){
             char buff[256];
-            sprintf(buff, "data/labels/%d_%d.png", i, j);
+            sprintf(buff, "../axl/darknet/data/labels/%d_%d.png", i, j);
             alphabets[j][i] = load_image_color(buff, 0, 0);
         }
     }
